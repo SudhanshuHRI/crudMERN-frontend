2540242b0d42b56691de16a596463395d7e3d8a8
SudhanshuMRM <sudhanshu@mrmprocom.com>
2024-09-27 12:13:50 +0530

    login ui completed

--- src/components/forgotComponent.js
+++ src/components/forgotComponent.js
@@ -3,25 +3,25 @@ import "../css/forgotComponent.css"
 
 const FrogotComponent = ({ switchForm }) => {
 
-    
+
     return (
         <>
-            <div>
-                <p className="h3">Forgot your Password?</p>
-                <p>Enter your Email or Mobile and we’ll help you reset your password.</p>
+            <div className='forgotContainer '>
+                <div className='mt-5 text-center'>
+                    <p className="h3">Forgot your Password?</p>
+                </div>
+                <div className='mt-5 text-center' style={{color:"gray"}}>
+                    <p className='h6 ps-5 px-5'>Enter your Email or Mobile and we’ll help you reset your password.</p>
+                </div>
+
                 <div className="inputDiv">
-                    <div className='loginInputs'>
+                    <div className='loginInputs mt-5 ps-2 px-2'>
                         <input type='text' placeholder='Enter email or mobile number' />
-
                     </div>
-                    <button className='btn registerButton mt-2 mb-2' onClick={()=>switchForm("verifyotp")}>send OTP</button>
-
-                    
+                    <button className='btn registerButton mt-5 mb-5' onClick={() => switchForm("verifyotp")}>Continue</button>
 
 
-
-
-                    <div className='d-flex justify-content-center'>
+                    <div className='d-flex justify-content-center returnLoginLink'>
                         <span>Return to </span>
                         <a href='#' className='ms-1' onClick={() => switchForm("login")}>Login</a>
                     </div>
--- src/components/verifyOTPComponent.js
+++ src/components/verifyOTPComponent.js
@@ -1,9 +1,38 @@
-const VarifyOTP = () => {
-    return ( 
-        <div>
-            <h1>This is VarifyOTP component</h1>
-        </div>
-     );
+import "../css/verifyOTPComponent.css"
+const VarifyOTP = ({ switchForm }) => {
+    
+    return (
+        <>
+            <div className='forgotContainer '>
+                <div className='mt-5 text-center'>
+                    <p className="h3">Two-Step Varification</p>
+                </div>
+                <div className="text-center mt-4 ">
+                    <img src="/otp.png" />
+                </div>
+                <div className='mt-5 text-center' style={{ color: "gray" }}>
+                    <p className='h5 ps-3 px-3'>Please enter the OTP (one time password) to verify your account. A Code has been sent to <span>+1*******179</span></p>
+                </div>
+
+                <div className="inputDiv">
+                    <div className='otpDiv  mt-5 ps-2 px-2 '>
+                        {/* <input type='text' placeholder='Enter email or mobile number' /> */}
+                        <input type="text"  maxLength="1"/>
+                        <input type="text" maxLength="1"/>
+                        <input type="text" maxLength="1"/>
+                        <input type="text" maxLength="1"/>
+                    </div>
+                    <button className='btn registerButton mt-5 mb-5' onClick={() => switchForm("verifyotp")}>Varify</button>
+
+
+                    <div className='d-flex justify-content-center '>
+                        <span>Return to </span>
+                        <a href='#' className='ms-1' onClick={() => switchForm("login")}>Login</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
- 
+
 export default VarifyOTP;
